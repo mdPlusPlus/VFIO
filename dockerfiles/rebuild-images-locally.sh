385aef53c5f4fe703ba42a3a421862c59b844ca9
mdPlusPlus <mdPlusPlus@users.noreply.github.com>
2019-08-20 00:02:00 +0200

    Modify local build script to enable inclusion of files.

--- dockerfiles/rebuild-images-locally.sh
+++ dockerfiles/rebuild-images-locally.sh
@@ -10,16 +10,16 @@ NAME_LIBVIRT_PYTHON="dpkg-buildpackage_libvirt-python_ubuntu"
 NAME_VIRT_MANAGER="dpkg-buildpackage_virt-manager_ubuntu"
 
 cat "${NAME_QEMU}/Dockerfile" \
-	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_QEMU}" - && \
+	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_QEMU}" -f - "${NAME_QEMU}" && \
 cat "${NAME_EDK2}/Dockerfile" \
 	| sed "s/FROM mdplusplus\/${NAME_QEMU}/FROM ${LOCAL_PREFIX}${NAME_QEMU}/" \
-	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_EDK2}" - && \
+	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_EDK2}" -f - "${NAME_EDK2}" && \
 cat "${NAME_LIBVIRT}/Dockerfile" \
 	| sed "s/FROM mdplusplus\/${NAME_QEMU}/FROM ${LOCAL_PREFIX}${NAME_QEMU}/"\
-	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_LIBVIRT}" - && \
+	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_LIBVIRT}" -f - "${NAME_LIBVIRT}" && \
 cat "${NAME_LIBVIRT_PYTHON}/Dockerfile" \
 	| sed "s/FROM mdplusplus\/${NAME_LIBVIRT}/FROM ${LOCAL_PREFIX}${NAME_LIBVIRT}/" \
-	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_LIBVIRT_PYTHON}" - && \
+	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_LIBVIRT_PYTHON}" -f - "${NAME_LIBVIRT_PYTHON}" && \
 cat "${NAME_VIRT_MANAGER}/Dockerfile" \
 	| sed "s/FROM mdplusplus\/${NAME_LIBVIRT_PYTHON}/FROM ${LOCAL_PREFIX}${NAME_LIBVIRT_PYTHON}/" \
-	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_VIRT_MANAGER}" -
+	| docker build ${NOCACHE} -t "${LOCAL_PREFIX}${NAME_VIRT_MANAGER}" -f - "${NAME_VIRT_MANAGER}"
